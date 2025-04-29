d5074df1929c91e4a15e95fd9cbdcc95e2cdd242
Faizan-Surani <suranifaizan52@gmail.com>
2025-04-29 17:08:35 +0530

    Changes in Orders Code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const authentication = (req, res, next) => {
-  const auth = req.headers["authorization"];
-  const token = auth && auth.split(" ")[1];
+  const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
     return res.status(401).json({ message: "Token is required!!" });
