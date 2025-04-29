d4f0133611068e86c93d347ee50f878b6c081c37
Faizan-Surani <suranifaizan52@gmail.com>
2025-04-29 19:14:32 +0530

    Changes in order logging  Code

--- server/routes/order.js
+++ server/routes/order.js
@@ -52,6 +52,7 @@ router.post("/placeOrder", authentication, async (req, res) => {
         quantity: item.quantity || 1,
       })),
       mode: "payment",
+      customer_email: req.user.email,
       success_url: `${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.FRONTEND_URL}/api/v1/cart`,
 
